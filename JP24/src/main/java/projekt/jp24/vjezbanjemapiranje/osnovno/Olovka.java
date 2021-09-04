64844b7621539b3d4b5ead091fbeb911b079a2f3
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-09-04 16:10:43 +0200

    mapiranje osovno

---
+++ JP24/src/main/java/projekt/jp24/vjezbanjemapiranje/osnovno/Olovka.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekt.jp24.vjezbanjemapiranje.osnovno;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author valagic
+ */
+@Entity
+public class Olovka {
+    
+    @Id
+    private Long id;
+    private String boja;
+    private String model;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBoja() {
+        return boja;
+    }
+
+    public void setBoja(String boja) {
+        this.boja = boja;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+    
+}
